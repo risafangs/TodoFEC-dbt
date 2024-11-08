a75ff13e5fd9726a05cfd32b75313fc7445f1616
even-wei <evenwei@infuseai.io>
2024-11-08 11:08:51 +0800

    Add model by state and candidate back
    Signed-off-by: even-wei <evenwei@infuseai.io>

---
+++ dbt/models/marts/advocacy_opposition_by_state_candidate.sql
@@ -0,0 +1,10 @@
+select
+    cand_name,
+    transaction_tp,
+    state,
+    count(distinct cmte_id) as cmte_count,
+    count(*) as transaction_count,
+    sum(transaction_amt) as total_amt
+from {{ ref('int_contributions_to_presidential_candidates') }}
+where transaction_tp in ('24A', '24E')
+group by cand_name, transaction_tp, state
--- dbt/models/marts/advocacy_opposition_by_swing_state.sql
+++ dbt/models/marts/advocacy_opposition_by_swing_state.sql
@@ -1,12 +1,4 @@
-select
-    cpc.cand_name,
-    cpc.transaction_tp,
-    cpc.state,
-    count(distinct cpc.cmte_id) as cmte_count,
-    count(*) as transaction_count,
-    sum(cpc.transaction_amt) as total_amt
-from {{ ref('int_contributions_to_presidential_candidates') }} cpc
+select ao.*
+from {{ ref('advocacy_opposition_by_state_candidate') }} ao
 join {{ ref('swing_states') }} ss
-    on cpc.state = ss.abbreviation
-where transaction_tp in ('24A', '24E')
-group by cpc.cand_name, cpc.transaction_tp, cpc.state
+    on ao.state = ss.abbreviation
